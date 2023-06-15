fce89a2fbc1a13029259e6fba5c91ebdffedce7e
frogcom <210587@student.glu.nl>
2023-06-15 21:23:06 +0200

    remove

--- tilesets/houses.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="houses" tilewidth="12" tileheight="12" tilecount="441" columns="21">
- <image source="houses.png" width="256" height="256"/>
-</tileset>
